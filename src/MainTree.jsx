a52f5fc981e3f7442c0254d421786ab3622a0533
ford-HUB <dyfordbonghanoy@gmail.com>
2025-03-25 14:58:57 +0800

    update the helper wrapper

--- src/MainTree.jsx
+++ src/MainTree.jsx
@@ -4,26 +4,33 @@ import Fallback from './pages/fallback'
 import Timeline from './pages/guest/Timeline'
 import Programs from './pages/guest/Programs'
 import UpComingEvents from './pages/guest/UpComingEvents'
+import LoginWrapper from './utils/LoginWrapper'
 
 const MainTree = [
     {
         path: '/',
-        element: <Home />,
+        element: <LoginWrapper>
+            <Home />
+        </LoginWrapper>,
         navbar: 'guest'
     },
     {
         path: '/Timeline',
-        element: <Timeline />,
+        element: <LoginWrapper></LoginWrapper>,
         navbar: 'guest'
     },
     {
         path: '/Programs',
-        element: <Programs />,
+        element: <LoginWrapper>
+            <Programs />
+        </LoginWrapper>,
         navbar: 'guest'
     },
     {
         path: '/UpComing-Events',
-        element: <UpComingEvents />,
+        element: <LoginWrapper>
+            <UpComingEvents />
+        </LoginWrapper>,
         navbar: 'guest'
     },
 
