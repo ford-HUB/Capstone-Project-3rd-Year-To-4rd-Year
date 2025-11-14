2d3971dc41458663aa7bb0885df56ff14310f257
Cris Dev. <dyfordbonghanoy@gmail.com>
2025-11-14 10:09:27 +0800

    fix the failling socket connection

--- frontend/src/api/socket.js
+++ frontend/src/api/socket.js
@@ -4,7 +4,7 @@ let socket;
 
 export const initSocket = () => {
     if (!socket) {
-        const mode = import.meta.env.MODE;
+        const mode = import.meta.env.VITE_SYS_MODE;
         const backendUrl = mode === 'development'
             ? import.meta.env.VITE_BACKEND_URL
             : import.meta.env.VITE_BACKEND_PROD;
