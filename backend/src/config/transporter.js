e9f8f64a99b0d321f36c3081cb7050176daf20cf
Cris Dev. <dyfordbonghanoy@gmail.com>
2025-11-15 00:04:17 +0800

    update the mailer google to production

--- backend/src/config/transporter.js
+++ backend/src/config/transporter.js
@@ -1,17 +1,12 @@
-import { Resend } from 'resend'
 import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const transporter = new Resend(process.env.MAILER_API_KEY)
-
-// export const transporter = nodemailer.createTransport({
-//     host: 'smtp.titan.email',
-//     port: 465,
-//     secure: true,
-//     auth: {
-//       user: process.env.AUTH_MAILER,
-//       pass: process.env.PASS_MAILER, 
-//   },
-// })
+export const transporter = nodemailer.createTransport({
+    host: 'gmail',
+    auth: {
+      user: process.env.AUTH_MAILER,
+      pass: process.env.PASS_MAILER, 
+  },
+})
