81aaa3eeb5925fcd22c30a0680bd966c81f7dff9
Cris Dev. <dyfordbonghanoy@gmail.com>
2025-11-20 04:35:35 +0800

    fix the un notice error

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv'
 import http from 'http'
 import { initSocket } from './socket.js'
 import { testConnection, dropTables, updateSchemaChanges } from './config/db.js'
-import fetch from 'node-fetch';
 
 // Import the cron job to start it
 import './cron/realtimeStatus.js'
@@ -37,7 +36,7 @@ const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
     console.log(`server running at http://localhost:${PORT}`)
     testConnection()
-    updateSchemaChanges()
+    // updateSchemaChanges()
     // dropTables()
     // simpleTest()
     // debugEmailSetup()
