b1a1cdc3831678afaa8d8541de75772b5bd373e7
Cris Dev. <dyfordbonghanoy@gmail.com>
2025-11-21 01:52:46 +0800

    comment out the updater schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,7 +36,7 @@ const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
     console.log(`server running at http://localhost:${PORT}`)
     testConnection()
-    updateSchemaChanges()
+    // updateSchemaChanges()
     // dropTables()
     // simpleTest()
     // debugEmailSetup()
