194a4315f2b8a11227c62aa47b31d5ea9ae2cdf7
One <ford-HUB@gmail.com>
2026-03-04 16:43:07 +0800

    remove: manually parsing since gemini have already json formatted response

--- backend/src/controllers/ai/gemini/gemini.controller.js
+++ backend/src/controllers/ai/gemini/gemini.controller.js
@@ -52,17 +52,17 @@ export const matchBeneficiaryLocationEvents = async (beneficiaryAddress, events,
             model: 'gemini-3.1-flash-lite-preview',
             contents: fullPrompt
         });
-        const rawResonse = result.candidates[0].content;
+        const contentResponse = result.candidates[0].content;
 
         console.log(typeof result.candidates[0].content);
         console.log(result.candidates[0].content);
 
-        const parsed = JSON.parse(rawResonse);
+        // const parsed = JSON.parse(rawResonse);
 
         return {
-            nearYouIds: parsed.near_you_events || [],
-            almostNearYouIds: parsed.almost_near_you_events || [],
-            recommendations: parsed.recommendations || []
+            nearYouIds: contentResponse.near_you_events || [],
+            almostNearYouIds: contentResponse.almost_near_you_events || [],
+            recommendations: contentResponse.recommendations || []
         };
     } catch (err) {
         console.error('Error in matchBeneficiaryLocationEvents:', err);
@@ -105,16 +105,16 @@ export const matchInterestedEvents = async (interest, events) => {
             model: 'gemini-3.1-flash-lite-preview',
             contents: fullPrompt
         });
-        const rawResonse = result.candidates[0].content;
+        const contentResponse = result.candidates[0].content;
 
         console.log(typeof result.candidates[0].content);
         console.log(result.candidates[0].content);
 
-        const parsed = JSON.parse(rawResonse);
+        // const parsed = JSON.parse(rawResonse);
 
         return {
-            matchedIds: parsed.matched_events || [],
-            recommendations: parsed.recommendations || []
+            matchedIds: contentResponse.matched_events || [],
+            recommendations: contentResponse.recommendations || []
         };
     } catch (err) {
         console.error('Error in matchInterestedEvents:', err);
