e58f3f43147802b70816a77c772b636445798249
One <ford-HUB@gmail.com>
2026-03-04 12:12:59 +0800

    fix:gemini env name

--- backend/src/config/gemini.js
+++ backend/src/config/gemini.js
@@ -1,4 +1,4 @@
 import 'dotenv/config'
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-export const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+export const genAI = new GoogleGenerativeAI(process.env.GEMINI_AI_API_KEY);
